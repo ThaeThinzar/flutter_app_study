ad75235510640fef3b8b1c90128fbdda19aff2b8
TTKS <thaethinzarkhaingsoe@techfun-myanmar.com>
2020-06-24 13:34:31 +0630

    [MaterialCompo_Kai] adding theme data

---
+++ lib/color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const pink50 = const Color(0xFFFEEAE6);
+const pink100 = const Color(0xFFFEDBD0);
+const pink300 = const Color(0xFFFBB8AC);
+const pink400 = const Color(0xFFEAA4A4);
+
+const Brown900 = const Color(0xFF442B2D);
+
+const ErrorRed = const Color(0xFFC5032B);
+
+const SurfaceWhite = const Color(0xFFFFFBFA);
+const  BackgroundWhite = Colors.white;
